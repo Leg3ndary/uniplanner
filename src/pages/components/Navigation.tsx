309c46aac953174cfb9acc6101269bd7c1d8c924
_Leg3ndary <ben.zhou.2006@gmail.com>
2024-02-09 17:29:31 -0500

    Adjustments

--- src/pages/components/Navigation.tsx
+++ src/pages/components/Navigation.tsx
@@ -2,10 +2,12 @@ import Link from "next/link";
 
 export default function Navigation() {
     return (
-        <nav className="flex items-center justify-between p-2 h-16 bg-sky-500 w-full">
-            <Link href="/">
-                <p className="font-black text-2xl p-4">UniPlanner</p>
-            </Link>
+        <nav className="flex justify-center items-center p-2 h-16 bg-sky-500 w-full">
+            <div className="flex 2xl:max-w-[1400px] w-full">
+                <Link href="/" className="self-start">
+                    <p className="font-black text-2xl p-4">UniPlanner</p>
+                </Link>
+            </div>
         </nav>
     );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
     return (
-        <main className="flex min-h-screen flex-col items-center justify-between p-2">
+        <main className="flex min-h-screen flex-col items-center p-2">
             <div className="flex items-center justify-center">
                 <h1>UniPlanner</h1>
             </div>
