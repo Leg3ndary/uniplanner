47880b3b63cec85a9df261b6ae75964e3f9331c1
_Leg3ndary <ben.zhou.2006@gmail.com>
2024-05-20 23:02:14 -0400

    Start adding stripe

---
+++ src/pages/checkout.tsx
@@ -0,0 +1,11 @@
+// import { loadStripe } from "@stripe/stripe-js";
+
+export default async function Checkout() {
+    // const stripe = await loadStripe(
+    //     process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!,
+    // );
+
+    return (
+        <p>well see</p>
+    );
+}
